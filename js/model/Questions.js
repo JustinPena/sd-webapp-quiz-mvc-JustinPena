2f61a814a69a100b80c34e56fbe4f72ccb75a2c1
JustinPena <302862351@student.rocmondriaan.nl>
2020-10-13 02:55:17 +0200

    Project oplevering.

---
+++ js/model/Questions.js
@@ -0,0 +1,82 @@
+export default  [
+{
+	"question": "24 / 4",
+	"option1" : "4",
+	"option2": "16",
+	"option3": "6",
+	"option4": "2",
+	"answer": 3
+},
+{
+	"question": "13 * 3",
+	"option1" : "13",
+	"option2": "203",
+	"option3": "39",
+	"option4": "63",
+	"answer": 3
+},
+{
+	"question": "9 - 0",
+	"option1" : "5",
+	"option2": "9",
+	"option3": "0",
+	"option4": "4",
+	"answer": 2
+},
+{
+	"question": "12 * 12",
+	"option1": "134",
+	"option2": "175",
+	"option3": "152",
+	"option4": "144",
+	"answer": 4
+},
+{
+	"question": "55 / 11",
+	"option1": "5",
+	"option2": "13",
+	"option3": "36",
+	"option4": "30",
+	"answer": 1
+},
+{
+	"question": "6 + 2 + 9",
+	"option1": "10",
+	"option2": "5",
+	"option3": "17",
+	"option4": "20",
+	"answer": 3
+},
+{
+	"question": "16 / 2",
+	"option1": "20",
+	"option2": "12",
+	"option3": "43",
+	"option4": "8",
+	"answer": 3
+},
+{
+	"question": "90 / 6",
+	"option1": "17",
+	"option2": "4",
+	"option3": "9",
+	"option4": "15",
+	"answer": 4
+},
+{
+	"question": "84 - 56",
+	"option1": "16",
+	"option2": "19",
+	"option3": "27",
+	"option4": "18",
+	"answer": 4
+},
+{
+	"question": "59 + 5",
+	"option1": "62",
+	"option2": "56",
+	"option3": "46",
+	"option4": "58",
+	"answer": 1
+}
+]
